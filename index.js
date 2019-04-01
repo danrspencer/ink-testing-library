5db5eecc73772838976be97b24e55b9c60179894
Dan Spencer <dan.spencer@anaplan.com>
2019-04-01 21:34:03 +0100

    Added a createRender wrapper function to allow mocking of the import mocking of ink

--- index.js
+++ index.js
@@ -1,66 +1,69 @@
-'use strict';
-const EventEmitter = require('events');
-const {render} = require('ink');
+"use strict";
+const EventEmitter = require("events");
+const { render } = require("ink");
 
 const createStdout = () => {
-	const frames = [];
-	let _lastFrame;
+  const frames = [];
+  let _lastFrame;
 
-	const stdout = {
-		write(frame) {
-			frames.push(frame);
-			_lastFrame = frame;
-		},
-		lastFrame() {
-			return _lastFrame;
-		},
-		frames,
-		columns: 100
-	};
+  const stdout = {
+    write(frame) {
+      frames.push(frame);
+      _lastFrame = frame;
+    },
+    lastFrame() {
+      return _lastFrame;
+    },
+    frames,
+    columns: 100
+  };
 
-	return stdout;
+  return stdout;
 };
 
 const createStdin = () => {
-	const stdin = new EventEmitter();
-	stdin.setEncoding = () => {};
-	stdin.setRawMode = () => {};
-	stdin.resume = () => {};
-	stdin.pause = () => {};
-	stdin.write = data => stdin.emit('data', data);
+  const stdin = new EventEmitter();
+  stdin.setEncoding = () => {};
+  stdin.setRawMode = () => {};
+  stdin.resume = () => {};
+  stdin.pause = () => {};
+  stdin.write = data => stdin.emit("data", data);
 
-	return stdin;
+  return stdin;
 };
 
 const instances = [];
 
-exports.render = tree => {
-	const stdout = createStdout();
-	const stdin = createStdin();
+const createRender = realRender => tree => {
+  const stdout = createStdout();
+  const stdin = createStdin();
 
-	const instance = render(tree, {
-		stdout,
-		stdin,
-		debug: true,
-		exitOnCtrlC: false
-	});
+  const instance = realRender(tree, {
+    stdout,
+    stdin,
+    debug: true,
+    exitOnCtrlC: false
+  });
 
-	instances.push(instance);
+  instances.push(instance);
 
-	return {
-		rerender: instance.rerender,
-		unmount: instance.unmount,
-		stdin: {
-			write: stdin.write
-		},
-		frames: stdout.frames,
-		lastFrame: stdout.lastFrame
-	};
+  return {
+    rerender: instance.rerender,
+    unmount: instance.unmount,
+    stdin: {
+      write: stdin.write
+    },
+    frames: stdout.frames,
+    lastFrame: stdout.lastFrame
+  };
 };
 
+exports.render = createRender(render);
+exports.createRender = createRender;
+
 exports.cleanup = () => {
-	for (const instance of instances) {
-		instance.unmount();
-		instance.cleanup();
-	}
+  for (const instance of instances) {
+    instance.unmount();
+    instance.cleanup();
+  }
 };
